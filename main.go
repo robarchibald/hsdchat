3974fceb2888a7c174fd3fed1eea025caa2e4f5a
Rob Archibald <rob.archibald@endfirst.com>
2017-01-31 18:12:28 -0800

    change default port. Serve on / rather than /ws

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 	"net/http"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
+var addr = flag.String("addr", ":921", "http service address")
 
 func main() {
 	flag.Parse()
 	hub := newHub()
 	go hub.run()
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
 	err := http.ListenAndServe(*addr, nil)
